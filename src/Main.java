8bb89c4bedd5a25d0c1655d29c93dcf5035a87ae
grwna <19623119@std.stei.itb.ac.id>
2025-02-18 00:15:48 +0700

    feat: create Main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    /*
+     TODO:
+     - Rename Variables
+     */
+    public static void main(String[] args) {
+        Input fileInputData = Input.handleFileInput();
+    }
+
+    
+
+
+}
+
